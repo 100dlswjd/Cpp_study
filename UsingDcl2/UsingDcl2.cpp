8e76892e9899e02a0fd411cccb2a9efc67ef958f
100dlswjd <82484962+100dlswjd@users.noreply.github.com>
2021-07-26 21:17:48 +0900

    Using

---
+++ UsingDcl2/UsingDcl2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main() {
+	int num = 20;
+	cout << "Hello World" << endl;
+	cout << "Hello " << "World" << endl;
+	cout << num << ' ' << 'A';
+	cout << ' ' << 3.14 << endl;
+	return 0;
+}
