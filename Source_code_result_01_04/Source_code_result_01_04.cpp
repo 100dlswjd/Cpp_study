a575ce3243d24fde6826dee1132c026b6d3a4b43
100dlswjd <82484962+100dlswjd@users.noreply.github.com>
2021-07-28 22:15:17 +0900

    test01_04

---
+++ Source_code_result_01_04/Source_code_result_01_04.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+int CalSalary(int sales) {
+	return (int)(50 + sales * 0.12);
+}
+
+int main(void) {
+	int sales;
+
+	while (1) {
+		std::cout << "판매 금액을 만원 단위로 입력 (-1 to end) : ";
+		std::cin >> sales;
+		if (sales == -1) {
+			break;
+		}
+
+		std::cout << "이번 달 급여 : ";
+		std::cout << CalSalary(sales) << "만원" << std::endl;
+	}
+	std::cout << "프로그램을 종료합니다." << std::endl;
+
+	return 0;
+}
