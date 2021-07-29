4fb942d6efae6f1c8382402dc0c6d7379f7b57cb
100dlswjd <82484962+100dlswjd@users.noreply.github.com>
2021-07-29 20:53:13 +0900

    SimpleFunc

---
+++ SimpleFunc/SimpleFunc.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include"SimpleFunc.h"
+
+void BestComImp1::SimpleFunc(void) {
+	std::cout << "BestCom이 정의한 함수" << std::endl;
+}
+
+void ProgComImp1::SimpleFunc(void) {
+	std::cout << "ProgCom이 정의한 함수" << std::endl;
+}
---
+++ SimpleFunc/SimpleMain.cpp
@@ -0,0 +1,6 @@
+#include"SimpleFunc.h"
+
+int main(void) {
+	BestComImp1::SimpleFunc();
+	ProgComImp1::SimpleFunc();
+}
