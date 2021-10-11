0cb6e55efeaf6fd48a0ccf34b2cf5f4f6ad23616
100dlswjd <100dlswjd@naver.com>
2021-10-11 18:30:18 +0900

    MyFriendClass

---
+++ MyFriendClass/MyFriendClass.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<cstring>
+
+class Girl;
+
+class Boy
+{
+private:
+	int height:
+	friend class Girl;
+public:
+	Boy(int len) : height(len)
+	{ }
+	void ShowYourFriendInfo(Girl& frn);
+};
+
+class Girl 
+{
+private:
+	char phNum[20];
+public:
+	Girl(char* num)
+	{
+		strcpy(phNum, num);
+	}
+	void ShowYourFriendInfo(Boy& frn);
+	friend class Boy;
+};
+
+void Boy::ShowYourFriendInfo(Girl& frn)
+{
+	std::cout << "Her phone number : " << frn.phNum << std::endl;
+}
+
+void Girl::ShowYourFriendInfo(Boy& frn)
+{
+	std::cout << "His height : " << frn.height << std::endl;
+}
+
+int main(void)
+{
+	Boy boy(170);
+	Girl girl("010-1234-5678");
+	boy.ShowYourFriendInfo(girl);
+	girl.ShowYourFriendInfo(boy);
+	return 0;
+}
