60a365c710682d3d00f1c537f553a3a511a97ef4
100dlswjd <100dlswjd@naver.com>
2021-10-12 15:39:01 +0900

    staticMember

--- StaticMember/StaticMember.cpp
+++ StaticMember/StaticMember.cpp
@@ -12,4 +12,34 @@ public:
 	}
 };
 
-int SoSimple::sim
+int SoSimple::simObjCnt = 0;
+
+class SoComplex
+{
+private:
+	static int cmxObjCnt;
+public:
+	SoComplex()
+	{
+		cmxObjCnt++;
+		std::cout << cmxObjCnt << "¹øÂ°  SoComplex °´Ã¼ " << std::endl;
+	}
+	SoComplex(SoComplex& copy)
+	{
+		cmxObjCnt++;
+		std::cout << cmxObjCnt << "¹øÂ° SoComplex °´Ã¼" << std::endl;
+	}
+};
+
+int SoComplex::cmxObjCnt = 0;
+
+int main(void)
+{
+	SoSimple sim1;
+	SoSimple sim2;
+
+	SoComplex cmx1;
+	SoComplex cmx2=cmx1;
+	SoComplex();
+	return 0;
+}
