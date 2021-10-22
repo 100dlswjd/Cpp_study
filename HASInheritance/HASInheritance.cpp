c829ef7117101581b0ec3d71a57ca08a61464664
100dlswjd <100dlswjd@naver.com>
2021-10-22 10:45:11 +0900

    HASInheritance

---
+++ HASInheritance/HASInheritance.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<cstring>
+
+class Gun
+{
+private:
+	int bullet;
+public:
+	Gun(int bnum) : bullet(bnum)
+	{ }
+	void Shot()
+	{
+		std::cout << "BBANG!" << std::endl;
+		bullet--;
+	}
+};
+
+class Police : public Gun
+{
+private:
+	int handcuffs;
+public:
+	Police(int bnum, int bcuff) : Gun(bnum), handcuffs(bcuff)
+	{ }
+	void PutHandcuff()
+	{
+		std::cout << "SNAP!" << std::endl;
+		handcuffs--;
+	}
+};
+
+int main(void)
+{
+	Police pman(5, 3);
+	pman.Shot();
+	pman.PutHandcuff();
+	return 0;
+}
