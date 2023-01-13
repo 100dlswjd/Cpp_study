7e213fd1419a7b687565582212ad87ee897eeb9f
100dlswjd <100dlswjd@naver.com>
2023-01-13 16:14:52 +0900

    이젝션 추가

--- globalHook/Injector/소스.cpp
+++ globalHook/Injector/소스.cpp
@@ -32,6 +32,47 @@ BOOL InjectDll(DWORD dwPID, std::wstring DllPath)
 	return TRUE;
 }
 
+BOOL EjectDll(DWORD dwPID, std::wstring DllPath)
+{
+	BOOL bMore = FALSE, bFound = FALSE;
+	HANDLE hSnapShot, hProcess, hThread;
+	HMODULE hModule = NULL;
+	MODULEENTRY32 me = { sizeof(me) };
+	LPTHREAD_START_ROUTINE pThreadProc;
+
+	hSnapShot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, dwPID);
+
+	bMore = Module32First(hSnapShot, &me);
+	for (; bMore; bMore = Module32Next(hSnapShot, &me))
+	{
+		if ((me.szExePath == DllPath) || (me.szModule == DllPath))
+		{
+			std::wcout << DllPath << std::endl;
+			std::wcout << "dll 찾음 " << me.szModule << std::endl;
+			bFound = TRUE;
+			break;
+		}
+	}
+	if (!bFound)
+	{
+		CloseHandle(hSnapShot);
+		return FALSE;
+	}
+
+	if (!(hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, dwPID)))
+		return FALSE;
+	
+	hModule = GetModuleHandle(L"kernel32.dll");
+	pThreadProc = (LPTHREAD_START_ROUTINE)GetProcAddress(hModule, "FreeLibrary");
+
+	hThread = CreateRemoteThread(hProcess, NULL, 0, pThreadProc, me.modBaseAddr, 0, NULL);
+	WaitForSingleObject(hThread, INFINITE);
+	CloseHandle(hThread);
+	CloseHandle(hProcess);
+	CloseHandle(hSnapShot);
+	return TRUE;
+}
+
 BOOL Injector(std::wstring ProcessName, std::wstring szDllPath, int injectMode)
 {
 	DWORD PID = 0;
@@ -46,20 +87,21 @@ BOOL Injector(std::wstring ProcessName, std::wstring szDllPath, int injectMode)
 	Process32First(hSnapShot, &pe);
 	do
 	{
-		PID = pe.th32ProcessID;
+		PID = pe.th32ProcessID;		
 		int result = (pe.szExeFile == ProcessName);
 
 		if (result)
 		{
+			std::cout << "PID : " << PID << std::endl;
 			if (injectMode == INJECT)
 			{
 				InjectDll(PID, szDllPath);
-				std::cout << "PID : " << PID << std::endl;
 				std::cout << "인젝션" << std::endl;
 			}
 			else
 			{
-				std::cout << "이젝션" << std::endl;
+				EjectDll(PID, szDllPath);
+				std::cout << "이젝션 "<< std::endl;
 			}
 		}
 
@@ -74,7 +116,8 @@ int main()
 	// UnHook86, UnHook64
 
 	std::wstring file = L"C:\\Users\\USER\\source\\repos\\globalHook\\x64\\Debug\\Hook64.dll";
-	std::cout << "Hello durige" << std::endl;
-	Injector(L"Taskmgr.exe", file, INJECT);
+	//file = L"Hook64.dll";
+	std::cout << "Hello world" << std::endl;
+	Injector(L"notepad.exe", file, EJECT);
 	return 0;
 }
