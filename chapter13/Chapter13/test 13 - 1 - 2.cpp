b065d707e03e02cf7c0ea862e74e6678430fa75e
100dlswjd <100dlswjd@naver.com>
2023-02-08 14:43:07 +0900

    1 - 2 문제 풀이

---
+++ chapter13/Chapter13/test 13 - 1 - 2.cpp
@@ -0,0 +1,22 @@
+/*
+#include <iostream>
+
+template <typename T>
+
+T SumArray(T arr[], int len)
+{
+	T sum = (T)0;
+	for (int i = 0; i < len; i++)
+		sum += arr[i];
+	return sum;
+}
+
+int main(void)
+{
+	int arr1[] = { 1, 2, 3, 4, 5 };
+	double arr2[] = { 1.1, 2.2, 3.3, 4.4, 5.5 };
+	std::cout << SumArray(arr1, sizeof(arr1) / sizeof(int)) << std::endl;
+	std::cout << SumArray(arr2, sizeof(arr2) / sizeof(double)) << std::endl;
+	return 0;
+}
+*/
