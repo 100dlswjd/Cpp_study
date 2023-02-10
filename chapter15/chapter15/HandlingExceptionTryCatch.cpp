d7636344e8073185d343b71f8ef828df7a862f6a
100dlswjd <100dlswjd@naver.com>
2023-02-10 14:17:07 +0900

    예외처리 메커니즘의 적용

---
+++ chapter15/chapter15/HandlingExceptionTryCatch.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main(void)
+{
+	int num1, num2;
+	std::cout << "두개의 숫자 입력 : ";
+	std::cin >> num1 >> num2;
+	try 
+	{
+		if (num2 == 0)
+			throw num2;
+		std::cout << "나눗셈의 몫 : " << num1 / num2 << std::endl;
+		std::cout << "나눗셈의 나머지 : " << num1 % num2 << std::endl;
+	}
+	catch (int expn)
+	{
+		std::cout << "제수는 " << expn << "이 될 수 없습니다." << std::endl;
+		std::cout << "프로그램을 다시 실행 하세요" << std::endl;
+	}
+	std::cout << "end of main" << std::endl;
+	return 0;
+}
--- chapter15/chapter15/UnhandlingException.cpp
+++ chapter15/chapter15/UnhandlingException.cpp
@@ -1,3 +1,4 @@
+/*
 #include <iostream>
 
 int main(void)
@@ -17,3 +18,4 @@ int main(void)
 	}
 	return 0;
 }
+*/
