97a8f058d98ac87c2f91b846de952f0ea89b334a
100dlswjd <100dlswjd@naver.com>
2023-02-14 13:41:45 +0900

    형변환 연산

---
+++ chapter16/chaper16/PowerfullCasting.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+class Car
+{
+private:
+	int fuelGauge;
+public:
+	Car(int fuel) : fuelGauge(fuel)
+	{  }
+	void ShowCarState() { std::cout << "잔여 연료량 : " << fuelGauge << std::endl; }
+};
+
+class Truck : public Car
+{
+private:
+	int freightWeight;
+
+public:
+	Truck(int fuel, int weight) : Car(fuel), freightWeight(weight)
+	{ }
+	void ShowTruckState()
+	{
+		ShowCarState();
+		std::cout << "화물의 무게 : " << freightWeight << std::endl;
+	}
+};
+
+int main(void)
+{
+	Car* pcar1 = new Truck(80, 200);
+	Truck* ptruck1 = (Truck*)pcar1;
+	ptruck1->ShowTruckState();
+	std::cout << std::endl;
+	Car* pcar2 = new Car(120);
+	Truck* ptruck2 = (Truck*)pcar2;
+	ptruck2->ShowTruckState();
+	return 0;
+}
