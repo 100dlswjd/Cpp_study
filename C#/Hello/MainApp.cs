8a0887cdff90673d78918b8ef273ccf2ef1df8a7
100dlswjd <100dlswjd@naver.com>
2023-06-29 00:04:18 +0900

    ㅇㅎ 이렇게

---
+++ C#/Hello/MainApp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using static System.Console;
+
+namespace Hello
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length == 0) 
+            {
+                Console.WriteLine("사용법 : Hello.exe <이름>");
+                return;
+            }
+            WriteLine("Hello, {0}", args[0]);
+        }
+    }
+}
