f797b9be54f22b712e7843b95b9e84b800b9f059
100dlswjd <100dlswjd@naver.com>
2023-07-03 22:38:39 +0900

    decimal 형식

--- C#/IntegralTypes/IntegralTypes/Program.cs
+++ C#/IntegralTypes/IntegralTypes/Program.cs
@@ -64,11 +64,15 @@ namespace IntegralTypes
         */
         static void Main(string[] args)
         {
-            float a = 3.1415_9265_3589_7932_3846f;
+            float a = 3.1415_9265_3589_7932_3846_2643_3832_79f;
             Console.WriteLine(a);
 
-            double b = 3.1415_9265_3589_7932_3846;
+            double b = 3.1415_9265_3589_7932_3846_2643_3832_79;
             Console.WriteLine(b);
+
+            decimal c = 3.1415_9265_3589_7932_3846_2643_3832_79m;
+            Console.WriteLine(c);
+
         }
     }
 }
