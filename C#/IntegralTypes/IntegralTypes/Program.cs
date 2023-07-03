35713bdc38611539fa020e2af690d86b3ff7cc8d
100dlswjd <100dlswjd@naver.com>
2023-07-03 21:46:50 +0900

    데이터 형식

---
+++ C#/IntegralTypes/IntegralTypes/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace IntegralTypes
+{    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            sbyte a = -10;
+            byte b = 40;
+
+            Console.WriteLine($"a={a}, b={b}");
+
+            short c = -30000;
+            ushort d = 60000;
+            Console.WriteLine($"c = {c}, d = {d}");
+
+            int e = -10_000_000; // 0이 7개
+            int f = 300_000_000; // 0이 8개
+            Console.WriteLine($"e = {e}, f = {f}");
+
+            long g = -500_000_000_000;
+            ulong h = 2_000_000_000_000_000_000;
+            Console.WriteLine($"g = {g}, h = {h}");
+        }
+    }
+}
