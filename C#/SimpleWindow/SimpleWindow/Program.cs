f1da11b0135ca50f1866a737ce440cc6cde0f21b
100dlswjd <100dlswjd@naver.com>
2023-07-04 12:29:44 +0900

    심플윈도우

---
+++ C#/SimpleWindow/SimpleWindow/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace SimpleWindow
+{
+    class MainApp : System.Windows.Forms.Form
+    {
+        static void Main(string[] args)
+        { 
+            System.Windows.Forms.Application.Run(new MainApp());
+        }
+    }
+}
