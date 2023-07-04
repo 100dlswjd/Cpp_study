6562354d538713174ce73832dbd1f56dcb0e3c43
100dlswjd <100dlswjd@naver.com>
2023-07-04 14:02:05 +0900

    Application 클래스

--- C#/SimpleWindow/SimpleWindow/Program.cs
+++ C#/SimpleWindow/SimpleWindow/Program.cs
@@ -1,9 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using Microsoft.VisualBasic;
 using System;
 using System.Windows.Forms;
 
+/*
 class MyForm : System.Windows.Forms.Form
 {
 }
@@ -24,5 +24,28 @@ namespace SimpleWindow
                 );
             Application.Run(form);
         }
+    }
+}
+*/
+
+namespace UsingApplication
+{
+    class MainApp : Form
+    {
+        static void Main(string[] args)
+        {
+            MainApp form = new MainApp();
+            form.Click += new EventHandler(
+                (sender, events) =>
+                {
+                    Console.WriteLine("Closing Window...");
+                    Application.Exit();
+                }
+                );
+            Console.WriteLine("Starting Window Application...");
+            //Application.Run(form);
+            Console.WriteLine("Exiting Window Application...");
+        }
+        
     }
 }
