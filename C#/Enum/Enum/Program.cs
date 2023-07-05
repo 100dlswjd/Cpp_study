4dfb35efd0c094f7a5b7ee18f27508c08851a88f
100dlswjd <100dlswjd@naver.com>
2023-07-05 19:32:02 +0900

    열거형식

---
+++ C#/Enum/Enum/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Enum
+{
+    class MainApp 
+    {
+        enum DialogResult { YES = 10, NO, CANCEL, CONFIRM = 50, OK}
+        static void Main(string[] args)
+        {
+            Console.WriteLine((int)DialogResult.YES);
+            Console.WriteLine((int)DialogResult.NO);
+            Console.WriteLine((int)DialogResult.CANCEL);
+            Console.WriteLine((int)DialogResult.CONFIRM);
+            Console.WriteLine((int)DialogResult.OK);
+
+            DialogResult result = DialogResult.YES;
+            Console.WriteLine(result == DialogResult.YES);
+            Console.WriteLine(result == DialogResult.NO);
+            Console.WriteLine(result == DialogResult.CANCEL);
+            Console.WriteLine(result == DialogResult.CONFIRM);
+            Console.WriteLine(result == DialogResult.OK);
+        }
+    }
+}
