e7356c05610cc6df601dde9794456c09679c88dd
100dlswjd <100dlswjd@naver.com>
2023-07-05 14:36:06 +0900

    char

---
+++ C#/Char/Char/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Char
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            char a = '안';
+            char b = '녕';
+            char c = '하';
+            char d = '세';
+            char e = '요';
+
+            Console.Write(a);
+            Console.Write(b);
+            Console.Write(c);
+            Console.Write(d);
+            Console.Write(e);
+            Console.WriteLine();
+        }
+    }
+}
