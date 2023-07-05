0244c1bd830a75679a7ad3ad3831f60c0862760a
100dlswjd <100dlswjd@naver.com>
2023-07-05 14:33:49 +0900

    float, double

---
+++ C#/FloatingPoint/FloatingPoint/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace FloatingPoint
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            float a = 3.1415_9265_3589_7932_3846f; // float 형식 변수에 값을 직접 할당하려면 숫자뒤에 f를 붙여야함
+            Console.WriteLine(a);
+
+            double b = 3.1415_9265_3589_7932_3846;
+            Console.WriteLine(b);
+        }
+    }
+}
