0fa4a2aeeb94490d9a469599b8810df32055031e
100dlswjd <100dlswjd@naver.com>
2023-07-06 13:28:56 +0900

    Nullable 형식

---
+++ C#/Nullable/Nullable/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Nullable
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            int? a = null;
+            Console.WriteLine(a.HasValue);
+            Console.WriteLine(a != null);
+
+            a = 3;
+            Console.WriteLine(a.HasValue);
+            Console.WriteLine(a != null);
+            Console.WriteLine(a.Value);
+        }
+    }
+}
