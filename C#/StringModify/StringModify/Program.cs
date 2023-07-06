20bbb5324ad7e588f19d23012f62b01d38cef55d
100dlswjd <100dlswjd@naver.com>
2023-07-06 21:02:25 +0900

    StringModify

---
+++ C#/StringModify/StringModify/Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+
+namespace StringModify
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("ToLower() : '{0}'", "ABC".ToLower());
+            WriteLine("ToUpper() : '{0}'", "abc".ToUpper());
+
+            WriteLine("Insert() : '{0}'", "Happy Friday!".Insert(5, " Sunny"));
+            WriteLine("Remove() : '{0}'", "I Don`t Love You.".Remove(2, 6));
+
+            WriteLine("Trim() : '{0}'", " No Spaces ".Trim());
+            WriteLine("TrimStart() : '{0}'", " No Spaces ".TrimStart());
+            WriteLine("TrimEnd() : '{0}'", " No Spaces ".TrimEnd());
+        }
+    }
+}
