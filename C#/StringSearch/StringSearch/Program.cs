fcb5cc45dcfd3c47c5d47c6b149e5a395afae606
100dlswjd <100dlswjd@naver.com>
2023-07-06 14:12:53 +0900

    StringSearch

---
+++ C#/StringSearch/StringSearch/Program.cs
@@ -0,0 +1,38 @@
+﻿using static System.Console;
+
+namespace StringSearch
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            string greeting = "Good Morning";
+            WriteLine(greeting);
+            WriteLine();
+
+            // IndexOf()
+
+            WriteLine("IndexOf 'Good' : {0}", greeting.IndexOf("Good"));
+            WriteLine("IndexOf 'o' : {0}", greeting.IndexOf('o'));
+
+            // LastIndexOf()
+            WriteLine("LastIndexOf 'Good' : {0}",greeting.LastIndexOf("Good"));
+            WriteLine("LastIndexOf 'o' : {0}", greeting.LastIndexOf('o'));
+
+            // StartsWith()
+            WriteLine("StartsWith 'Good' : {0}", greeting.StartsWith("Good"));
+            WriteLine("StartsWith 'Morning' : {0}", greeting.StartsWith("Morning"));
+
+            // EndsWith()
+            WriteLine("EndsWith 'Good' : {0}", greeting.EndsWith("Good"));
+            WriteLine("EndsWith 'Morning' : {0}", greeting.EndsWith("Morning"));
+
+            // Contains()
+            WriteLine("Contains 'Evening' : {0}", greeting.Contains("Evening"));
+            WriteLine("Contains 'Morning' : {0}", greeting.Contains("Morning"));
+
+            // Replaced()
+            WriteLine("Replaced 'Morning' with 'Evening' : {0}", greeting.Replace("Morning", "Evening"));
+        }
+    }
+}
