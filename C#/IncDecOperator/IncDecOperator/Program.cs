8c832642a1fe7eb98f211f6af4620fa9d2c860c5
100dlswjd <100dlswjd@naver.com>
2023-07-07 17:38:11 +0900

    IncDecOperator StringConcatenate

---
+++ C#/IncDecOperator/IncDecOperator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace IncDecOperator
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            Console.WriteLine(a++);
+            Console.WriteLine(++a);
+
+            Console.WriteLine(a--);
+            Console.WriteLine(--a);
+
+            string result = "123" + "456";
+            Console.WriteLine(result);
+            result = "Hello" + " " + "World";
+            Console.WriteLine(result);
+        }
+    }
+}
