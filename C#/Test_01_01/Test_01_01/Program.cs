d2afb18c89823906790ae1bbbe9061bd25b333b7
100dlswjd <100dlswjd@naver.com>
2023-07-07 16:22:16 +0900

    1번문제 풀기

---
+++ C#/Test_01_01/Test_01_01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Test_01_01
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("사각형의 너비를 입력하세요 ! : ");
+            string width = Console.ReadLine();
+
+            Console.WriteLine("사각형의 높이를 입력하세요 ! : ");
+            string height = Console.ReadLine();
+
+            Console.WriteLine("사각형의 넓이는 : {0}", Convert.ToInt32(width) * Convert.ToInt32(height));
+        }
+    }
+}
