2843e4933b2de7be5e9a04673d2595980c5f4b6c
100dlswjd <100dlswjd@naver.com>
2023-07-07 14:57:24 +0900

    StringFormatDatetime

---
+++ C#/StringFormatDatetime/StringFormatDatetime/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+using static System.Console;
+
+namespace StringFormatDatetime
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2023, 07, 07, 14, 34, 22);
+
+            WriteLine("12시간 형식 : {0:yyyy-MM-dd tt hh:mm:ss (ddd)}", dt);
+            WriteLine("24시간 형식 : {0:yyyy-MM-dd HH:mm:ss (dddd)}", dt);
+
+            CultureInfo ciKo = new CultureInfo("ko_KR");
+
+            WriteLine(dt.ToString("yyyy-MM-dd tt hh:mm:ss (ddd)", ciKo));
+            WriteLine(dt.ToString("yyyy-MM-dd HH:mm:ss (dddd)", ciKo));
+            WriteLine(dt.ToString(ciKo));
+
+            CultureInfo ciEn = new CultureInfo("en-US");
+            WriteLine(dt.ToString("yyyy-MM-dd tt hh:mm:ss (ddd)", ciEn));
+            WriteLine(dt.ToString("yyyy-MM-dd HH:mm:ss (dddd)", ciEn));
+            WriteLine(dt.ToString(ciEn));
+        }
+    }
+}
