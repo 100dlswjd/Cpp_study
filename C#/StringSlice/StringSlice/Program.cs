bd1277ed84323687b09185a42acfed6b442644df
100dlswjd <100dlswjd@naver.com>
2023-07-07 13:06:21 +0900

    문자열 분할

---
+++ C#/StringSlice/StringSlice/Program.cs
@@ -0,0 +1,24 @@
+﻿using static System.Console;
+
+namespace StringSlice
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            string greeting = "Good morning.";
+            WriteLine(greeting.Substring(0, 5));
+            WriteLine(greeting.Substring(5));
+            WriteLine();
+
+            String[] arr = greeting.Split(new string[] {" "}, StringSplitOptions.None);
+
+            WriteLine("Word Count : {0}", arr.Length);
+
+            foreach (string element in arr)
+            {
+                WriteLine("{0}", element);
+            }
+        }
+    }
+}
