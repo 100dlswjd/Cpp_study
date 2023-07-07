172ebbe4578c19500f756e70d946347035d38be9
100dlswjd <100dlswjd@naver.com>
2023-07-07 13:40:43 +0900

    String Format Basic

---
+++ C#/StringFormatBasic/StringFormatBasic/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+
+namespace StringFormatBasic
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            string fmt = "{0, -20}{1,-15}{2,30}";
+
+            WriteLine(fmt, "Publisher", "Author", "Title");
+            WriteLine(fmt, "marvel", "Stan Lee", "Iron Man");
+            WriteLine(fmt, "Hanbit", "Sanghyun Park", "This is C#");
+            WriteLine(fmt, "Prentice Hall", "K&R", "The C Programming Language");
+        }
+    }
+}
