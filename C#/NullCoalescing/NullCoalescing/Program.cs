6c37e5cb2080928f2d1a38c9bb8c211674eae47e
100dlswjd <100dlswjd@naver.com>
2023-07-17 13:32:25 +0900

    null 병합연산자

---
+++ C#/NullCoalescing/NullCoalescing/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace NullCoalescing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int? num = null;
+            Console.WriteLine($"{num ?? 0}");
+
+            num = 99;
+            Console.WriteLine($"{num ?? 0}");
+
+            string s = null;
+            Console.WriteLine($"{s ?? "Default"}");
+
+            s = "Specific";
+            Console.WriteLine($"{s ?? "Default"}");
+        }
+    }
+}
