12fb40bc9ab1539abaccbf14d79740b0cbd8dab4
100dlswjd <100dlswjd@naver.com>
2023-07-20 18:40:48 +0900

    switch2

---
+++ C#/Switch/Switch/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Switch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("요일을 입력하세요(월, 화, 수, 목, 금, 토, 일) :");
+            string day = Console.ReadLine();
+
+            switch (day)
+            {
+                case "월":
+                    Console.WriteLine("Monday");
+                    break;
+
+                case "화":
+                    Console.WriteLine("Tuesday");
+                    break;
+
+                case "수":
+                    Console.WriteLine("Wednesday");
+                    break;
+
+                case "목":
+                    Console.WriteLine("Thursday");
+                    break;
+
+                case "금":
+                    Console.WriteLine("Friday");
+                    break;
+
+                case "토":
+                    Console.WriteLine("Saturday");
+                    break;
+
+                case "일":
+                    Console.WriteLine("Sunday");
+                    break;
+            }
+        }
+    }
+}
---
+++ C#/Switch2/Switch2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Switch2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            object obj = null;
+
+            string s = Console.ReadLine();
+
+            if (int.TryParse(s, out int out_i))
+            {
+                obj = out_i;
+            }
+            else if (float.TryParse(s, out float out_f))
+            {
+                obj = out_f;
+            }
+            else
+            {
+                obj = s;
+            }
+
+            switch (obj)
+            {
+                case int:
+                    Console.WriteLine($"{(int)obj}는 int 형식입니다.");
+                    break;
+                case float:
+                    Console.WriteLine($"{(float)obj}는 float 형식입니다.");
+                    break;
+                default:
+                    Console.WriteLine($"{obj}(은)는 모르는 형식입니다.");
+                    break;
+            }
+            
+        }
+    }
+}
